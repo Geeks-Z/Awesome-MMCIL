727fe874f42d85c6a8342c9e7936bbac28444d6f
Geeks_Z <izhw1024@163.com>
2025-09-06 21:24:56 +0800

    RAPF

--- RAPF-main/train.sh
+++ RAPF-main/train.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 python main.py \
     --config-path configs/class \
+    --config-name cifar100_10-10.yaml \
+    dataset_root="/home/team/zhaohongwei/Dataset" \
+    class_order="class_orders/cifar100_order.yaml"
+python main.py \
+    --config-path configs/class \
     --config-name imagenet_r_10-10.yaml \
     dataset_root="/home/team/zhaohongwei/Dataset/imagenet-r" \
     class_order="class_orders/imagenet_R_order.yaml"
-
-#python main.py \
-#    --config-path configs/class \
-#    --config-name cifar100_10-10.yaml \
-#    dataset_root="/home/team/zhaohongwei/Dataset" \
-#    class_order="class_orders/cifar100_order.yaml"
+python main.py \
+    --config-path configs/class \
+    --config-name imagenet100_10-10.yaml \
+    dataset_root="/home/team/zhaohongwei/Dataset" \
+    class_order="class_orders/imagenet100.yaml"
 
